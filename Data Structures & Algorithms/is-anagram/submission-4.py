f9d811be61421190d22c493d1fbe4e128df91350
Josh Beira <126598855+joshbeira@users.noreply.github.com>
2026-07-09 13:15:19 +0100

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+
+
+    # my sol: 
+    # def isAnagram(self, s: str, t: str) -> bool:
+    #     chars = []
+    #     if len(s) == len(t):
+    #         for i in s:
+    #             chars.append(i)
+    #         for i in t:
+    #             if i in chars:
+    #                 try: 
+    #                     chars.remove(i)
+    #                 except: 
+    #                     return False 
+    #             else: 
+    #                 return False 
+    #     else:
+    #         return False 
+
+    #     return True
+    
